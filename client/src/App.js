0a5067ab23efa7d9441d78d069eecc6a2aa1dd30
shreyashingle2004@gmail.com <shreyashingle2004@gmail.com>
2023-10-22 22:06:33 +0530

    last commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { Toaster } from "react-hot-toast";
+import { themeSettings } from "./theme";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Summary from "./pages/Summary";
+import Paragraph from "./pages/Paragraph";
+import ChatBot from "./pages/ChatBot";
+import JsConverter from "./pages/JsConverter";
+import ScifiImage from "./pages/ScifiImage";
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings()), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/summary" element={<Summary />} />
+          <Route path="/paragraph" element={<Paragraph />} />
+          <Route path="/chatbot" element={<ChatBot />} />
+          <Route path="/js-converter" element={<JsConverter />} />
+          <Route path="/scifi-image" element={<ScifiImage />} />
+        </Routes>
+      </ThemeProvider>
+    </>
   );
 }
 
